eb49969915c1496f3c1e198190f7258224299323
boomgreen87 <boomgreen87@yahoo.com>
2019-03-25 16:47:16 -0400

    Used greensock timeline to animate SVG

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 (() => {
-	let badge = document.querySelector('img');
+	let myTL = new TimelineLite();
 
-	function animImage() {
-		TweenMax.to(badge, 2.5, {scaleX: 1.025, scaleY: 1.025, rotation: 50, ease:Elastic.easeInOut, onComplete: animFooterText});
-	}
+	myTL.to("#background", 0.5, {rotation: 1000, scaleX: 1.1, scaleY: 1.1, transformOrigin: "50% 50%"})
+		.to("#small_type", 1, {rotation: -360, transformOrigin: "50% 50%"});
 
-	function animFooterText() {
-		TweenMax.to('h3', 0.7, {scaleX: 2, scaleY: 2, ease:Elastic.easeOut});
-	}
-
-	// animImage();
-	badge.addEventListener('mouseover', animImage);
 })();
